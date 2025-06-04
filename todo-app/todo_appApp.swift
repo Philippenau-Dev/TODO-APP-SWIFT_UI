00ba6c473708161b12430137d7ca11f0f89b7556
Philippe Nau Rosa <philippe.rosa@premiersoft.net>
2025-06-04 09:21:05 -0300

    Initial Commit

---
+++ todo-app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo-app
+//
+//  Created by Premiersoft on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
